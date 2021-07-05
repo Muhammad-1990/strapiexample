3fb47c2c32a7636fa717ced186f2e6eb253f85d0
Muhammad Ahmod <muhammadahmod@Muhammads-MacBook.local>
2021-07-05 16:42:23 +0200

    db ssl removal test

--- config/database.js
+++ config/database.js
@@ -10,11 +10,10 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        }, 
+        //ssl: env.bool('DATABASE_SSL', false),
+        //ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)} // For self-signed certificates }
       },
-      options: {}
+      options: {},
     },
   },
 });
